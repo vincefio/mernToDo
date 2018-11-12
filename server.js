85106b99398c93030d1dce34e39678052e05694a
Vince <vincentfiorilli@gmail.com>
2018-11-12 18:45:46 -0500

    remove package.lock

--- server.js
+++ server.js
@@ -24,4 +24,14 @@ db.once('open', function () {
     console.log(`MongoDB database connected!`)
 });
 
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 app.listen(port, () => console.log(`App listening on port ${port}`))
